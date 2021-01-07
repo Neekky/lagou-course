c5d53b59f640c4a6fcf7709309ea9ecdecb71be6
曾超 <475383200@qq.com>
2021-01-07 20:29:55 +0800

    feat: 提交代码

--- 01-01-study -materials/01-01-codes/01-01-01-01-functional-programming/17-compose-debug.js
+++ 01-01-study -materials/01-01-codes/01-01-01-01-functional-programming/17-compose-debug.js
@@ -7,7 +7,7 @@ const _ = require('lodash')
 //   console.log(v)
 //   return v
 // }
-
+const compose = (...args) => value => args.reverse().reduce((acc, fn) => fn(acc), value)
 const trace = _.curry((tag, v) => {
   console.log(tag, v)
   return v
@@ -19,8 +19,10 @@ const split = _.curry((sep, str) => _.split(str, sep))
 // _.toLower()
 const join = _.curry((sep, array) => _.join(array, sep))
 
+// 转换为数组
 const map = _.curry((fn, array) => _.map(array, fn))
 
-const f = _.flowRight(join('-'), trace('map 之后'), map(_.toLower), trace('split 之后'), split(' '))
+
+const f = compose(join('-'), trace('map 之后'), map(_.toLower), trace('split 之后'), split(' '))
 
 console.log(f('NEVER SAY DIE'))
