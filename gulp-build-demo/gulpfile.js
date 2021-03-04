25a8521f5a9b74c9fe049ca1fabcfc3e08a86d8e
曾超 <475383200@qq.com>
2021-03-04 22:51:12 +0800

    feat: 提交

--- gulp-build-demo/gulpfile.js
+++ gulp-build-demo/gulpfile.js
@@ -15,96 +15,103 @@ const bs = browserSync.create();
 
 const data = {
   menus: [{
-    name: 'Home',
-    icon: 'aperture',
-    link: 'index.html'
-  },
-  {
-    name: 'Features',
-    link: 'features.html'
-  },
-  {
-    name: 'About',
-    link: 'about.html'
-  },
-  {
-    name: 'Contact',
-    link: '#',
-    children: [{
-      name: 'Twitter',
-      link: 'https://twitter.com/w_zce'
+      name: 'Home',
+      icon: 'aperture',
+      link: 'index.html'
+    },
+    {
+      name: 'Features',
+      link: 'features.html'
     },
     {
       name: 'About',
-      link: 'https://weibo.com/zceme'
+      link: 'about.html'
     },
     {
-      name: 'divider'
-    },
-    {
-      name: 'About',
-      link: 'https://github.com/zce'
+      name: 'Contact',
+      link: '#',
+      children: [{
+          name: 'Twitter',
+          link: 'https://twitter.com/w_zce'
+        },
+        {
+          name: 'About',
+          link: 'https://weibo.com/zceme'
+        },
+        {
+          name: 'divider'
+        },
+        {
+          name: 'About',
+          link: 'https://github.com/zce'
+        }
+      ]
     }
-    ]
-  }
   ],
   pkg: require('./package.json'),
   date: new Date()
 }
 
 const clean = () => {
-  return del(['dist'])
+  return del(['dist', 'temp'])
 }
 
 const style = () => {
   return src('src/assets/styles/*.scss', {
-    base: 'src'
-  })
+      base: 'src'
+    })
     .pipe(plugins.sass({
       outputStyle: 'expanded'
     }))
-    .pipe(dest('dist'))
+    .pipe(dest('temp'))
+    .pipe(bs.reload({stream: true}))
 }
 
 const script = () => {
   return src('src/assets/scripts/*.js', {
-    base: 'src'
-  })
+      base: 'src'
+    })
     .pipe(plugins.babel({
       presets: ['@babel/preset-env']
     }))
-    .pipe(dest('dist'))
+    .pipe(dest('temp'))
+    .pipe(bs.reload({stream: true}))
 }
 
 const page = () => {
   return src('src/*.html', {
-    base: 'src'
-  })
+      base: 'src'
+    })
     .pipe(plugins.swig({
       data,
-      defaults: { cache: false }
+      defaults: {
+        cache: false // 防止模板缓存导致页面不能及时更新
+      }
     }))
-    .pipe(dest('dist'))
+    .pipe(dest('temp'))
+    .pipe(bs.reload({stream: true}))
 }
 
 const image = () => {
   return src('src/assets/images/**', {
-    base: 'src'
-  })
+      base: 'src'
+    })
     .pipe(plugins.imagemin())
     .pipe(dest('dist'))
 }
 
 const font = () => {
   return src('src/assets/fonts/**', {
-    base: 'src'
-  })
+      base: 'src'
+    })
     .pipe(plugins.imagemin())
     .pipe(dest('dist'))
 }
 
 const extra = () => {
-  return src('public/**', { base: 'public' })
+  return src('public/**', {
+      base: 'public'
+    })
     .pipe(dest('dist'))
 }
 
@@ -115,7 +122,7 @@ const serve = () => {
   // watch('src/assets/images/**', image)
   // watch('src/assets/fonts/**', font)
   // watch('public/**', extra)
-  
+
   watch([
     'src/assets/images/**',
     'src/assets/fonts/**',
@@ -128,7 +135,7 @@ const serve = () => {
     open: true,
     files: 'dist/**',
     server: {
-      baseDir: ['dist', 'src', 'public'],
+      baseDir: ['temp', 'src', 'public'],
       // routes可以手动配置请求优先路径
       routes: {
         "/node_modules": "node_modules"
@@ -137,18 +144,32 @@ const serve = () => {
   })
 }
 
+const useref = () => {
+  return src('temp/*.html', {
+      base: 'temp'
+    })
+    .pipe(plugins.useref({
+      searchPath: ['temp', '.']
+    }))
+    // 分别处理html、css、js
+    .pipe(plugins.if(/\.js$/, plugins.uglify()))
+    .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+    .pipe(plugins.if(/\.html$/, plugins.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    })))
+    .pipe(dest('dist'));
+}
 
+const compile = parallel(style, script, page );
 
-const compile = parallel(style, script, page,);
-
-const build = series(clean, parallel(compile, image, font, extra));
+const build = series(clean, parallel(series(compile, useref), image, font, extra));
 
 const develop = series(compile, serve)
 
 module.exports = {
   develop,
-  compile,
   build,
   clean,
-  serve
 }
