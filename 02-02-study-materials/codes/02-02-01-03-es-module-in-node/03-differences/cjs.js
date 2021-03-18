c334c0ac5eff9da751db9211f7ffe3a2ab997176
曾超 <475383200@qq.com>
2021-03-18 08:03:27 +0800

    feat: 代码提交

--- 02-02-study-materials/codes/02-02-01-03-es-module-in-node/03-differences/cjs.js
+++ 02-02-study-materials/codes/02-02-01-03-es-module-in-node/03-differences/cjs.js
@@ -1,11 +1,11 @@
-// 加载模块函数
-console.log(require)
+// // 加载模块函数
+// console.log(require)
 
-// 模块对象
-console.log(module)
+// // 模块对象
+// console.log(module)
 
-// 导出对象别名
-console.log(exports)
+// // 导出对象别名
+// console.log(exports)
 
 // 当前文件的绝对路径
 console.log(__filename)
--- 02-02-study-materials/codes/02-02-01-03-es-module-in-node/03-differences/esm.mjs
+++ 02-02-study-materials/codes/02-02-01-03-es-module-in-node/03-differences/esm.mjs
@@ -1,19 +1,19 @@
 // ESM 中没有模块全局成员了
 
-// // 加载模块函数
-// console.log(require)
+// 加载模块函数
+console.log(require)
 
-// // 模块对象
-// console.log(module)
+// 模块对象
+console.log(module)
 
-// // 导出对象别名
-// console.log(exports)
+// 导出对象别名
+console.log(exports)
 
-// // 当前文件的绝对路径
-// console.log(__filename)
+// 当前文件的绝对路径
+console.log(__filename)
 
-// // 当前文件所在目录
-// console.log(__dirname)
+// 当前文件所在目录
+console.log(__dirname)
 
 // -------------
 
