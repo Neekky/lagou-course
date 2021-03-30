78a39fbea9d326c7323c528a933ff9cfa667d78e
曾超 <475383200@qq.com>
2021-03-30 20:11:50 +0800

    feat: 上传rollup代码

---
+++ rollup/src/index.js
@@ -0,0 +1,22 @@
+// 导入模块成员
+import _ from "lodash-es";
+import {
+    log
+} from './logger';
+import messages from './messages';
+import {
+    name,
+    version
+} from "../package.json";
+
+import cjs from "./cjs-module";
+
+// 使用模块成员
+const msg = messages.hi;
+
+log(msg);
+
+console.log(name);
+console.log(version);
+console.log(_.camelCase('hello world'));
+console.log(cjs);
